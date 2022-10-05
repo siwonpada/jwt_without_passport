9eda79f930915d508b3670f812e6bc13622b8c8b
siwonpada <psw041115@gmail.com>
2022-10-05 17:26:00 +0900

    set_jwtmodule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configservice: ConfigService) => ({
+        secret: configservice.get<string>('JWT_SECRET'),
+        expiresIn: configservice.get<string>('JWT_EXPIRES'),
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
